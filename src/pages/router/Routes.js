d6704da954e6d5b5dc45a253e01840480480b740
채윤병 <ybchae0801@naver.com>
2021-03-02 08:37:53 +0900

    feat : 마이페이지 추가

--- src/pages/router/Routes.js
+++ src/pages/router/Routes.js
@@ -1,21 +1,23 @@
 import React from "react";
 import {BrowserRouter, Route} from "react-router-dom";
 import Index from "../main/Index";
+import MyPage from "../user/MyPage";
 import BookDetailPage from "../main/BookDetailPage";
 import BookAddPage from "../main/BookAddPage";
 
 class Routes extends React.Component {
-  render() {
-    return (
-      <div>
-        <BrowserRouter>
-          <Route exact path="/" component={Index} />
-          <Route exact path="/books/new" component={BookAddPage} />
-          <Route exact path="/books/:bookId" component={BookDetailPage} />
-        </BrowserRouter>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Route exact path="/" component={Index}/>
+                    <Route exact path="/myPage" component={MyPage}/>
+                    <Route exact path="/books/new" component={BookAddPage}/>
+                    <Route exact path="/books/:bookId" component={BookDetailPage}/>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default Routes;
