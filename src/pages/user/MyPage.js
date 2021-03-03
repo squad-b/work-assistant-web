31e7c33e677efdbea08958db9249ccd60884422a
채윤병 <ybchae0801@naver.com>
2021-03-03 23:55:10 +0900

    feat : 마이 페이지 만들기
    user 정보 store에 하드코딩

--- src/pages/user/MyPage.js
+++ src/pages/user/MyPage.js
@@ -2,34 +2,14 @@ import React from "react";
 import Profile from "../../components/user/Profile";
 import BorrowingBookList from "../../components/user/BorrowingBookList";
 
-class MyPage extends React.Component {
-  constructor(props) {
-    super(props);
-    const rentalDate = new Date().toDateString();
-    this.state = {
-      user: {
-        profile: {
-          name: '채윤병',
-          email: 'yunbchae@miridih.com'
-        },
-        bookList: [
-          {id: 1, title: 'title 1', rentalDate: rentalDate},
-          {id: 2, title: 'title 2', rentalDate: rentalDate},
-          {id: 3, title: 'title 3', rentalDate: rentalDate}
-        ]
-      }
-    }
-  }
-
+export default class MyPage extends React.Component {
   render() {
     return (
       <div>
         <h1>마이 페이지</h1>
-        <Profile profile={this.state.user.profile}/>
-        <BorrowingBookList bookList={this.state.user.bookList} />
+        <Profile profile={this.props.user.profile}/>
+        <BorrowingBookList bookList={this.props.user.bookList} />
       </div>
     );
   }
 }
-
-export default MyPage;
