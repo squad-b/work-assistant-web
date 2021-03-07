c64199c2f600dd564a1e139cc4b45387b68e260c
유인근 <keun0390@naver.com>
2021-03-08 01:37:12 +0900

    chore: BookCard 컴포넌트 상태, state로 변경

--- src/components/main/BookCard.js
+++ src/components/main/BookCard.js
@@ -5,29 +5,31 @@ import { Link } from "react-router-dom";
 class BookCard extends React.Component {
   constructor(props) {
     super(props);
-    this.book = {
-      id: this.props.book.id,
-      title: this.props.book.title,
-      imageUrl: this.props.book.imageUrl,
-      stockQuantity: this.props.book.stockQuantity
+    this.state = {
+      book: {
+        id: this.props.book.id,
+        title: this.props.book.title,
+        imageUrl: this.props.book.imageUrl,
+        stockQuantity: this.props.book.stockQuantity
+      }
     }
   }
 
   getBorrowableStatusClassName = () => {
-    return this.book.stockQuantity > 0 ? "borrow borrow-enable" : "borrow borrow-unable";
+    return this.state.book.stockQuantity > 0 ? "borrow borrow-enable" : "borrow borrow-unable";
   }
 
   getBorrowableStatus = () => {
-    return this.book.stockQuantity > 0 ? "대출 가능" : "대출 불가";
+    return this.state.book.stockQuantity > 0 ? "대출 가능" : "대출 불가";
     // return this.book.stockQuantity > 0 ? `대출 가능(${this.book.stockQuantity}권)` : "대출 불가";
   }
 
   render() {
     return (
       <div className="book-card">
-        <Link to={'/books/' + this.book.id}>
-            <img className="book-thumbnail" src={this.book.imageUrl}></img>
-              <p className="book-title">{this.book.title}</p>
+        <Link to={'/books/' + this.state.book.id}>
+            <img className="book-thumbnail" src={this.state.book.imageUrl}></img>
+              <p className="book-title">{this.state.book.title}</p>
 
               <p className={this.getBorrowableStatusClassName()}>{this.getBorrowableStatus()}</p>
         </Link>
