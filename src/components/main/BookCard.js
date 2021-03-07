b3d3619d4a505fe18e42c1fa1e609ddf33d9f7d9
유인근 <keun0390@naver.com>
2021-03-07 22:31:00 +0900

    feat: 카테고리별 책 목록 정렬

--- src/components/main/BookCard.js
+++ src/components/main/BookCard.js
@@ -8,34 +8,22 @@ class BookCard extends React.Component {
     this.book = {
       id: this.props.book.id,
       title: this.props.book.title,
-      subtitle: this.props.book.subtitle,
-      thumbnail: this.props.book.thumbnail,
-      categories: this.props.book.categories,
-      borrower: this.props.book.borrower,
+      imageUrl: this.props.book.imageUrl,
+      stockQuantity: this.props.book.stockQuantity
     }
   }
 
-  getBorrowStatus = () => {
-    return this.book.borrower === "" ? "대츨 가능" : "대출 불가";
-  }
-
   getBorrowStatusClassName = () => {
-    return this.book.borrower === "" ? "borrow-enable" : "borrow-unable"; 
+    return this.book.stockQuantity > 0 ? "borrow-enable" : "borrow-unable"; 
   }
 
   render() {
     return (
       <div className="book-card">
         <Link to={'/books/' + this.book.id}>
-          <p>제목: {this.book.title}</p>
-          <p>부제목: {this.book.subtitle}</p>
-          <img className="book-thumbnail" src={this.book.thumbnail}></img>
-          <p> 카테고리: 
-            {this.book.categories.map((category) => {
-              return <span>{category} </span>
-            })}
-          </p>
-          <p className={this.getBorrowStatusClassName()}>{this.getBorrowStatus()}</p>
+          <img className="book-thumbnail" src={this.book.imageUrl}></img>
+          <p className="book-title">{this.book.title}</p>
+          <p className={this.getBorrowStatusClassName()}>{this.book.stockQuantity}</p>
         </Link>
       </div>
     );
--- src/components/main/BookListSection.js
+++ src/components/main/BookListSection.js
@@ -5,122 +5,197 @@ import "./book-list-section.scss";
 class BookListSection extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      bookList: [
-        {
-            id: 1,
-            title: "밥 잘하는 법",
-            subtitle: "밥이 이렇게 맛있다고?",
-            thumbnail: "http://image.kyobobook.co.kr/images/book/large/741/l9791185473741.jpg",
-            categories: ["개발", "밥"],
-            borrower: "",
-            startDate: "",
-            endDate: ""
-        },
-        {
-            id: 2,
-            title: "돈의 속성",
-            subtitle: "최상위 부자가 말하는 돈에 대한 모든 것",
-            thumbnail: "http://image.yes24.com/goods/90428162/800x0",
-            categories: ["돈", "부자"],
-            borrower: "유인근",
-            startDate: "",
-            endDate: ""
-        },
-        {
-            id: 3,
-            title: "돈의 속성",
-            subtitle: "최상위 부자가 말하는 돈에 대한 모든 것",
-            thumbnail: "http://image.yes24.com/goods/90428162/800x0",
-            categories: ["돈", "부자"],
-            borrower: "",
-            startDate: "",
-            endDate: ""
-        },
-        {
-            id: 4,
-            title: "돈의 속성",
-            subtitle: "최상위 부자가 말하는 돈에 대한 모든 것",
-            thumbnail: "http://image.yes24.com/goods/90428162/800x0",
-            categories: ["돈", "부자"],
-            borrower: "",
-            startDate: "",
-            endDate: ""
-        },
-        {
-            id: 5,
-            title: "돈의 속성",
-            subtitle: "최상위 부자가 말하는 돈에 대한 모든 것",
-            thumbnail: "http://image.yes24.com/goods/90428162/800x0",
-            categories: ["돈", "부자"],
-            borrower: "유인근",
-            startDate: "",
-            endDate: ""
-        },
-        {
-            id: 6,
-            title: "돈의 속성",
-            subtitle: "최상위 부자가 말하는 돈에 대한 모든 것",
-            thumbnail: "http://image.yes24.com/goods/90428162/800x0",
-            categories: ["돈", "부자"],
-            borrower: "",
-            startDate: "",
-            endDate: ""
-        },
-        {
-            id: 7,
-            title: "돈의 속성",
-            subtitle: "최상위 부자가 말하는 돈에 대한 모든 것",
-            thumbnail: "http://image.yes24.com/goods/90428162/800x0",
-            categories: ["돈", "부자"],
-            borrower: "",
-            startDate: "",
-            endDate: ""
-        },
-        {
-            id: 8,
-            title: "돈의 속성",
-            subtitle: "최상위 부자가 말하는 돈에 대한 모든 것",
-            thumbnail: "http://image.yes24.com/goods/90428162/800x0",
-            categories: ["돈", "부자"],
-            borrower: "",
-            startDate: "",
-            endDate: ""
-        },
-        {
-            id: 9,
-            title: "돈의 속성",
-            subtitle: "최상위 부자가 말하는 돈에 대한 모든 것",
-            thumbnail: "http://image.yes24.com/goods/90428162/800x0",
-            categories: ["돈", "부자"],
-            borrower: "",
-            startDate: "",
-            endDate: ""
-        },
-        {
-            id: 10,
-            title: "돈의 속성",
-            subtitle: "최상위 부자가 말하는 돈에 대한 모든 것",
-            thumbnail: "http://image.yes24.com/goods/90428162/800x0",
-            categories: ["돈", "부자"],
-            borrower: "",
-            startDate: "",
-            endDate: ""
-        }
-      ]
+    // TODO: 인근, API 호출로 책 목록 가져오기 
+    this.bookList = [
+      {
+        id: 1,
+        title: "자바 ORM 표준 JPA 프로그래밍",
+        imageUrl: "https://simage.kyobobook.co.kr/images/book/large/330/l9788960777330.jpg",
+        category: "DEVELOP",
+        stockQuantity: 1
+      },
+      {
+        id: 2,
+        title: "구글이 목표를 달성하는 방식 OKR",
+        imageUrl: "http://image.kyobobook.co.kr/images/book/large/276/l9788947544276.jpg",
+        category: "MANAGEMENT",
+        stockQuantity: 2
+      },
+      {
+        id: 3,
+        title: "프레젠테이션 잘하는 법",
+        imageUrl: "http://image.kyobobook.co.kr/images/book/large/427/l9788984053427.jpg",
+        category: "PLAN,SELF_IMPROVEMENT",
+        stockQuantity: 0
+      },
+      {
+        id: 4,
+        title: "구글이 목표를 달성하는 방식 OKR",
+        imageUrl: "http://image.kyobobook.co.kr/images/book/large/276/l9788947544276.jpg",
+        category: "MANAGEMENT",
+        stockQuantity: 1
+      },
+      {
+        id: 5,
+        title: "자바 ORM 표준 JPA 프로그래밍",
+        imageUrl: "https://simage.kyobobook.co.kr/images/book/large/330/l9788960777330.jpg",
+        category: "DEVELOP",
+        stockQuantity: 2
+      },
+      {
+        id: 6,
+        title: "구글이 목표를 달성하는 방식 OKR",
+        imageUrl: "http://image.kyobobook.co.kr/images/book/large/276/l9788947544276.jpg",
+        category: "MANAGEMENT",
+        stockQuantity: 1
+      },
+      {
+        id: 7,
+        title: "자바 ORM 표준 JPA 프로그래밍",
+        imageUrl: "https://simage.kyobobook.co.kr/images/book/large/330/l9788960777330.jpg",
+        category: "DEVELOP",
+        stockQuantity: 2
+      },
+      {
+        id: 8,
+        title: "자바 ORM 표준 JPA 프로그래밍",
+        imageUrl: "https://simage.kyobobook.co.kr/images/book/large/330/l9788960777330.jpg",
+        category: "DEVELOP",
+        stockQuantity: 0
+      },
+      {
+        id: 9,
+        title: "자바 ORM 표준 JPA 프로그래밍",
+        imageUrl: "https://simage.kyobobook.co.kr/images/book/large/330/l9788960777330.jpg",
+        category: "DEVELOP",
+        stockQuantity: 1
+      },
+      {
+        id: 10,
+        title: "구글이 목표를 달성하는 방식 OKR",
+        imageUrl: "http://image.kyobobook.co.kr/images/book/large/276/l9788947544276.jpg",
+        category: "MANAGEMENT",
+        stockQuantity: 1
+      },
+      {
+        id: 11,
+        title: "프레젠테이션 잘하는 법",
+        imageUrl: "http://image.kyobobook.co.kr/images/book/large/427/l9788984053427.jpg",
+        category: "PLAN,SELF_IMPROVEMENT",
+        stockQuantity: 1
+      },
+      {
+        id: 12,
+        title: "프레젠테이션 잘하는 법",
+        imageUrl: "http://image.kyobobook.co.kr/images/book/large/427/l9788984053427.jpg",
+        category: "PLAN,SELF_IMPROVEMENT",
+        stockQuantity: 2
+      },
+      {
+        id: 13,
+        title: "자바 ORM 표준 JPA 프로그래밍",
+        imageUrl: "https://simage.kyobobook.co.kr/images/book/large/330/l9788960777330.jpg",
+        category: "DEVELOP",
+        stockQuantity: 3
+      },
+      {
+        id: 14,
+        title: "자바 ORM 표준 JPA 프로그래밍",
+        imageUrl: "https://simage.kyobobook.co.kr/images/book/large/330/l9788960777330.jpg",
+        category: "DEVELOP",
+        stockQuantity: 0
+      },
+      {
+        id: 15,
+        title: "광고의 8원칙",
+        imageUrl: "http://image.kyobobook.co.kr/images/book/large/500/l9791197220500.jpg",
+        category: "MARKETING",
+        stockQuantity: 1
+      },
+      {
+        id: 16,
+        title: "세브란스 병원 이야기",
+        imageUrl: "http://image.kyobobook.co.kr/images/book/large/500/l9791197220500.jpg",
+        category: "NONFICTION",
+        stockQuantity: 0
+      },
+      {
+        id: 17,
+        title: "광고의 8원칙",
+        imageUrl: "http://image.kyobobook.co.kr/images/book/large/500/l9791197220500.jpg",
+        category: "MARKETING",
+        stockQuantity: 0
+      },
+      {
+        id: 18,
+        title: "멋진 신세계",
+        imageUrl: "http://image.kyobobook.co.kr/images/book/large/725/l9788973814725.jpg",
+        category: "FICTION",
+        stockQuantity: 2
+      },
+      {
+        id: 19,
+        title: "아이디어가 고갈된 디자이너를 위한 책 : 로고 디자인 편",
+        imageUrl: "http://image.kyobobook.co.kr/images/book/large/050/l9791190357050.jpg",
+        category: "DESIGN",
+        stockQuantity: 0
+      },
+      {
+        id: 20,
+        title: "컴퓨터활용능력 1급 실기(2021)",
+        imageUrl: "http://image.kyobobook.co.kr/images/book/large/152/l9791165213152.jpg",
+        category: "LICENSE",
+        stockQuantity: 0
+      }
+    ];
+    this.bookListByCategory = this.getBookListByCategory();
+    this.bookCategory = {
+      "DEVELOP": "개발",
+      "MANAGEMENT": "경영",
+      "PLAN": "기획",
+      "MARKETING": "마켓팅",
+      "SELF_IMPROVEMENT": "자기계발",
+      "LICENSE": "자격증",
+      "DESIGN": "디자인",
+      "FICTION": "소설",
+      "NONFICTION": "비소설"
     }
-  }  
+  }
 
-  componentDidMount() {
-    // API 호출로 bookList 데이터 셋팅 예정
-    // this.state.bookList = axios.get();
+  getBookListByCategory = () => {
+    const bookListByCategory = {};
+
+    this.bookList.forEach((book) => {
+      const categories = book.category.split(",");
+
+      categories.forEach((category) => {
+        if (bookListByCategory[category] === undefined) {
+          bookListByCategory[category] = [book];
+        } else {
+          bookListByCategory[category].push(book);
+        }
+      })
+    })
+
+    return bookListByCategory;
   }
 
   render() {
     return (
-      <div className="book-list-section">
-        {this.state.bookList.map((book, i) => {
-          return <BookCard book={book} key={i}/>
+      <div>
+        {Object.keys(this.bookCategory).map((key, i) => {          
+          return (
+            <div> 
+              <div>
+                <h3 className="book-category">{this.bookCategory[key]}</h3>
+              </div>
+              <div className="book-list-section">
+                {this.bookListByCategory[key].map((book, j) => {
+                  return <BookCard book={book} key={j}></BookCard>
+                })}
+              </div>
+            </div>
+          )
         })}
       </div>
     );
