c621d43cedcad641a8454fa7fc6bbf3d4161d66b
5minho <5minhominho@gmail.com>
2021-03-25 22:07:30 +0900

    feature #34 책 등록 api 와 연동중

--- src/components/book/add/BookAddPopup.js
+++ src/components/book/add/BookAddPopup.js
@@ -13,6 +13,7 @@ import {
   TextField
 } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import api from "../../../api";
 
 class BookAddPopup extends React.Component {
 
@@ -31,46 +32,62 @@ class BookAddPopup extends React.Component {
       this.setState({bookCategory : bookCategory})
     }
 
+    const onClickRegisterButton = async (book) => {
+      console.log(book.isbn)
+      const res = await api.post("/books", {
+        isbn: book.isbn.split(' ')[0],
+        title: book.title,
+        description: book.contents,
+        author: book.authors[0],
+        publishingDate: book.datetime,
+        publisher: book.publisher,
+        category: this.state.bookCategory,
+        imageUrl: book.thumbnail,
+        stockQuantity: this.state.stockQuantity
+      });
+      console.log(res)
+      alert(res.data)
+    }
 
     const {open, book, onClose} = this.props
 
     return (
-        <Dialog open={open} fullWidth={true} maxWidth='sm' aria-labelledby={"book-add-popup-title"} onClose={onClose}>
-          <DialogTitle id={"book-add-popup-title"}>책 등록</DialogTitle>
-          <DialogContent>
-            <form className={'book-add-popup-form'} noValidate>
-              <DialogContentText>
-                책 이름
-              </DialogContentText>
-              <img className={'book-thumbnail'} src={book ? book.thumbnail: 'https://resource.miricanvas.com/image/common/design-history-preview-placeholder.png'}/>
-              <FormControl className={'form-control'}>
-                <InputLabel htmlFor='category'>책 카테고리</InputLabel>
-                <Select autoFocus value={this.state.bookCategory} inputProps={{name:'book-category', id:'book-category'}} onChange={onChange}>
-                  <MenuItem value={'DEVELOP'}>개발</MenuItem>
-                  <MenuItem value={'MANAGEMENT'}>경영</MenuItem>
-                  <MenuItem value={'PLAN'}>기획</MenuItem>
-                  <MenuItem value={'MARKETING'}>마케팅</MenuItem>
-                  <MenuItem value={'SELF_IMPROVEMENT'}>자기계발</MenuItem>
-                  <MenuItem value={'LICENSE'}>자격증</MenuItem>
-                  <MenuItem value={'DESIGN'}>디자인</MenuItem>
-                  <MenuItem value={'FICTION'}>소설</MenuItem>
-                  <MenuItem value={'NONFICTION'}>비소설</MenuItem>
-                </Select>
-              </FormControl>
-              <TextField
-                id="standard-number"
-                label="수량"
-                type="number"
-                value={this.state.stockQuantity}
-              />
-            </form>
-            <DialogActions>
-              <Button>
-                등록하기
-              </Button>
-            </DialogActions>
-          </DialogContent>
-        </Dialog>
+      <Dialog open={open} fullWidth={true} maxWidth='sm' aria-labelledby={"book-add-popup-title"} onClose={onClose}>
+        <DialogTitle id={"book-add-popup-title"}>책 등록</DialogTitle>
+        <DialogContent>
+          <form className={'book-add-popup-form'} noValidate>
+            <DialogContentText>
+              책 이름
+            </DialogContentText>
+            <img className={'book-thumbnail'} src={book ? book.thumbnail: 'https://resource.miricanvas.com/image/common/design-history-preview-placeholder.png'}/>
+            <FormControl className={'form-control'}>
+              <InputLabel htmlFor='category'>책 카테고리</InputLabel>
+              <Select autoFocus value={this.state.bookCategory} inputProps={{name:'book-category', id:'book-category'}} onChange={onChange}>
+                <MenuItem value={'DEVELOP'}>개발</MenuItem>
+                <MenuItem value={'MANAGEMENT'}>경영</MenuItem>
+                <MenuItem value={'PLAN'}>기획</MenuItem>
+                <MenuItem value={'MARKETING'}>마케팅</MenuItem>
+                <MenuItem value={'SELF_IMPROVEMENT'}>자기계발</MenuItem>
+                <MenuItem value={'LICENSE'}>자격증</MenuItem>
+                <MenuItem value={'DESIGN'}>디자인</MenuItem>
+                <MenuItem value={'FICTION'}>소설</MenuItem>
+                <MenuItem value={'NONFICTION'}>비소설</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              id="standard-number"
+              label="수량"
+              type="number"
+              value={this.state.stockQuantity}
+            />
+          </form>
+          <DialogActions>
+            <Button onClick={() => {onClickRegisterButton(book)}}>
+              등록하기
+            </Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
     )
   }
 }
