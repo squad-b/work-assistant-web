3b40d16b37e3b42a2f3b3f25775911e275650ad5
유인근 <keun0390@naver.com>
2021-04-01 00:44:08 +0900

    chore: 띄어쓰기 포맷팅

--- src/components/book/add/BookSearchResultList.js
+++ src/components/book/add/BookSearchResultList.js
@@ -8,15 +8,15 @@ class BookSearchResultList extends React.Component {
   render() {
     const {searchBooks, onClickCard} = this.props;
     return (
-        <div className="book-list-section">
-          <div className={searchBooks.length <= 0 ? "no-result" : (searchBooks.length <= 5 ? "less-than-five-item-result" : "more-than-five-item-result")}>
-            {searchBooks.map((book, idx) => {
-              return (
-                  <BookCard book={book} key={idx} register={true} onClick={onClickCard}/>
-              )
-            })}
-          </div>
+      <div className="book-list-section">
+        <div className={searchBooks.length <= 0 ? "no-result" : (searchBooks.length <= 5 ? "less-than-five-item-result" : "more-than-five-item-result")}>
+          {searchBooks.map((book, idx) => {
+            return (
+                <BookCard book={book} key={idx} register={true} onClick={onClickCard}/>
+            )
+          })}
         </div>
+      </div>
     )
   }
 }
