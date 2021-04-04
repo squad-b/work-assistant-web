f95e3daeab3fc2d827ab9fb4bd1687a2b550fafd
5minho <5minhominho@gmail.com>
2021-04-04 21:33:26 +0900

    feature #47: 로그아웃하고 로그인 페이지로 돌아가기 시도 중

--- src/components/common/NavItem.js
+++ src/components/common/NavItem.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import "./navitem.scss"
 
 class NavItem extends React.Component {
   render() {
     return (
-      <li className="main-nav-item">
+      <li className="main-nav-item" onClick={this.props.navItem.onClick} >
         <Link to={this.props.navItem.link}>{this.props.navItem.item}</Link>
       </li>
     );
