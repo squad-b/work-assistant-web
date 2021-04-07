79c23dcce7c57e8f4e45806997ab00c1eb705166
채윤병 <ybchae0801@naver.com>
2021-04-07 23:02:18 +0900

    feat: #50 책 반납 API 연동하기
    - 반납 도서마다 반납 API 호출

--- src/components/user/BorrowingBookList.js
+++ src/components/user/BorrowingBookList.js
@@ -3,17 +3,40 @@ import {Box} from "@material-ui/core";
 import {DataGrid} from "@material-ui/data-grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import api from "../../api";
+import store from "../../store";
 
 class BorrowingBookList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      rows: this.props.bookList
+      rows: this.props.bookList,
+      selectedRentals: []
     }
   }
 
-  returnBooks() {
-    alert('반납요');
+  selectRentals = (rentalList) => {
+    this.setState({selectedRentals: rentalList.selectionModel});
+  }
+
+  returnBooks = async () => {
+    const returnList = this.state.selectedRentals;
+    if (returnList.length < 1) {
+      alert('반납할 도서를 선택해주세요 📖');
+      return;
+    }
+    // TODO: 윤병, 한번에 여러 도서를 반납 가능하도록 수정 필요
+    for (const rentalId of returnList) {
+      await this.returnBook(rentalId);
+    }
+    const response = await api.get(`/members/${store.getState().memberId}/rentals`)
+    const borrowingBookList = response.data;
+    this.setState({ borrowingBookList: borrowingBookList });
+  }
+
+  returnBook = async (rentalId) => {
+    const response = await api.put(`/rentals/${rentalId}`, {status: 'RETURN'});
+    if (response.result !== 'SUCCESS') console.log(`책 반납 중 error 발생. rentalId: ${rentalId}`);
   }
 
   render() {
@@ -32,7 +55,13 @@ class BorrowingBookList extends React.Component {
           대여 목록
         </Typography>
         <Box style={{height: 350, width: '100%'}} mt={2}>
-          <DataGrid rows={bookList} columns={columns} pageSize={5} checkboxSelection/>
+          <DataGrid
+            rows={bookList}
+            columns={columns}
+            pageSize={5}
+            checkboxSelection
+            onSelectionModelChange={this.selectRentals.bind(this)}
+          />
           <Button
             type="submit"
             fullWidth
