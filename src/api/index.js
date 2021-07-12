980feb617a06ef9ff9ef3b5eb10bdb090bfcb8a5
유인근 <keun0390@naver.com>
2021-07-12 22:49:31 +0900

    config: api ec2 ip 주소 추가

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/",
+  // baseURL: "http://localhost:8080/", // localhost
+  baseURL: "http://13.125.144.148/", // api ec2 ip
   headers: {
     "content-type": "application/json"
   },
